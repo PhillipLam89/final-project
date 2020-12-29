d811263da49c6991dba2ee627b7a269257df4c29
Phillip Lam <plam89@ucla.edu>
2020-12-28 17:55:15 -0800

    improved bootstrap code

--- client/navBar.jsx
+++ client/navBar.jsx
@@ -3,7 +3,7 @@ export default class NavBar extends React.Component {
 
   render() {
     return (
-      <nav className="navbar navbar-light bottom-nav">
+      <nav className="navbar navbar-light bottom-nav mb-0 pb-0 d-flex justify-content-space-around">
         <img src="./images/home.png" width="30" height="30" className="d-inline-block align-top" alt=""></img>
         <img src="./images/search.png" width="30" height="30" className="d-inline-block align-top" alt=""></img>
         <img src="./images/settings.png" width="30" height="30" className="d-inline-block align-top" alt=""></img>
--- client/search.jsx
+++ client/search.jsx
@@ -53,9 +53,7 @@ class Search extends React.Component {
         return response.json();
       })
       .then(data => {
-
         const cityId = data.suggestions[0].entities[0].destinationId;
-
         fetch(`/api/search/list/${cityId}/${this.state.ratingFilter}`).then(response => response.json()).then(data => {
           const hotelList = data.data.body.searchResults.results;
           this.setState({ userInput: this.state.userInput, hotelList: hotelList, searchButtonClicked: 'hidden', isLoading: false, ratingFilter: this.state.ratingFilter });
@@ -72,29 +70,29 @@ class Search extends React.Component {
     const { hotelList, userInput, searchButtonClicked, userInputError, ratingFilter } = this.state;
     const displayedList = hotelList.map((hotel, idx) => {
       return (
-        <div className="hotel-display-div" key={hotel.supplierHotelId}>
+        <div className="hotel-display-div text-center d-flex flex-column justify-content-center align-items-center" key={hotel.supplierHotelId}>
           <p key={hotelList.id} className="hotel-name">{hotel.name}</p>
-          <img key={hotel.thumbnailUrl} src={hotel.thumbnailUrl} className="hotel-img"></img>
+          <img key={hotel.thumbnailUrl} src={hotel.thumbnailUrl} className="hotel-img pb-2"></img>
         </div>
       );
     });
     return (
       <div>
-        <div className={searchButtonClicked === '' ? 'hidden' : '' + ' arrow-div'}>
-          <img onClick={this.handleBackClick} className="back-arrow" width="50rem" src="https://cdn0.iconfinder.com/data/icons/navigation-set-arrows-part-one/32/ChevronLeftCircle-512.png"></img>
-          <p className="search-txt"> Back to Search Screen</p>
+        <div className={searchButtonClicked === '' ? 'd-none' : '' + ' arrow-div align-items-center pt-2 d-flex justify-content-start'}>
+          <img onClick={this.handleBackClick} className="back-arrow " width="50rem" src="https://cdn0.iconfinder.com/data/icons/navigation-set-arrows-part-one/32/ChevronLeftCircle-512.png"></img>
+          <p className="search-txt text-center pt-0.5 pl-1"> Back to Search Screen</p>
         </div>
-        <main className={searchButtonClicked === '' ? 'hidden' : ''}>
-          <div className="header"><h3>{userInput.toUpperCase() + ` ${this.state.ratingFilter}-STAR HOTELS:`}</h3></div>
-          <div className="hotel-list">{displayedList}</div>
+        <main className={searchButtonClicked === '' ? 'd-none' : ''}>
+          <div className="header text-center"><h3>{userInput.toUpperCase() + ` ${this.state.ratingFilter}-STAR HOTELS:`}</h3></div>
+          <div className="hotel-list d-flex flex-column p-1.5">{displayedList}</div>
         </main>
         <div className={searchButtonClicked}>
           <img className="main-pic" src="https://cdn.cnn.com/cnnnext/dam/assets/190903131748-greek-luxury-seaside-hotels---grecotel-mykonos-blu---infinity-pool-1.jpg"></img>
-          <header className={searchButtonClicked}>
-            <nav className={userInputError ? 'navbar navbar-light bg-light input-container error-border' : 'navbar navbar-light bg-light input-container'}>
+          <header className={`${searchButtonClicked} text-center`}>
+            <nav className={userInputError ? 'navbar navbar-light bg-light input-container error-border d-flex' : 'navbar navbar-light bg-light input-container'}>
               <form onSubmit={this.handleSearchClick} className={'form-inline'}>
                 <input value={userInput} onChange={this.handleUserInput} className="form-control mr-sm-2" type="search" placeholder="city (worldwide)" aria-label="Search"></input>
-                <div className="form-row align-items-center preference-div">
+                <div className="form-row align-items-center preference-div pl-0.5">
                   <div className="col-auto my-1">
                     <select value={ratingFilter} onChange={this.ratingFilter} className="custom-select mr-sm-2" id="inlineFormCustomSelect">
                       <option value="" defaultValue>Filter By Rating</option>
@@ -109,7 +107,7 @@ class Search extends React.Component {
                   </div>
                 </div>
                 <button className="search-btn btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                <h6 className="user-error">{userInputError && !ratingFilter ? 'CHOOSE A RATING & VALID CITY' : ''}</h6>
+                <h6 className="user-error pl-0.5">{userInputError && !ratingFilter ? 'CHOOSE A RATING & VALID CITY' : ''}</h6>
                 <h6>{userInputError && ratingFilter ? <p className="user-error">INVALID CITY</p> : ''}</h6>
               </form>
             </nav>
