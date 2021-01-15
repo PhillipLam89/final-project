289bb6eec223fb647232fb302f681b1489318b3e
Phillip Lam <plam89@ucla.edu>
2021-01-14 18:40:45 -0800

    added responsiveness so that hotels display in a single column on  mobile, 2 columns on desktop

--- client/searchResults.jsx
+++ client/searchResults.jsx
@@ -28,9 +28,9 @@ export default class SearchResults extends React.Component {
     const hotelList = this.state.hotelList;
     const hotelName = hotelList.map((hotel, idx) => {
       return (
-        <div className="row-cols-md-1 p-3  card-deck card-img-top d-flex justify-content-center">
-          <div className="card  border border-dark">
-            <img key={hotel.thumbnailUrl} src={hotel.thumbnailUrl} className="hotel-img pb-2 hotel-pic"></img>
+        <div className="row-cols-md-1 p-3 col-12 card-deck card-img-top d-flex justify-content-center hotel-info-container">
+          <div className="card  border border-dark ">
+            <img key={hotel.thumbnailUrl} src={hotel.thumbnailUrl} className="hotel-img pb-2"></img>
             <div className="card-body">
             <a href={`#hotel-details?hotelId=${hotelList[idx].id}?thumbnailUrl=${hotel.thumbnailUrl}`}><div key={hotelList[idx].id} className="hotel-name">{hotel.name}</div></a>
               <p className="card-text d-flex justify-content-between">
@@ -47,7 +47,7 @@ export default class SearchResults extends React.Component {
         <main className="w-100">
           <div className="header text-center"><h3>{`${this.props.cityName.toUpperCase()} ${this.props.ratingFilter}-STAR HOTELS:`}</h3></div>
         </main>
-        <div className="row row-cols-2 row-cols-md-2 h-50 m-0 d-flex justify-content-center results-container">
+        <div className="row row-cols-2 row-cols-md-2 h-50 m-0 mb-5 d-flex justify-content-center results-container ">
           {hotelName}
         </div>
         <NavBar />
