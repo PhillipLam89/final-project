f7d4d890849dce7c3ac7da546ad37a56c418042a
Phillip Lam <plam89@ucla.edu>
2021-02-04 02:35:14 -0800

    displayed book divs for review

--- client/SeeReviews.jsx
+++ client/SeeReviews.jsx
@@ -40,30 +40,32 @@ export default class SeeReviews extends React.Component {
        const reviews = this.state.allHotelsReviewed.map((reviewedHotel, index) => {
 
          return (
-
-           <div key={index+1} className=" p-2  d-flex justify-content-center hotel-info-container">
-             <div key={index+2} className="card shadow-sm">
-               <div key={index + 3} className="card-body">
-                 <a key={index + 4}><div className="hotel-name">{reviewedHotel.hotelName}</div></a>
-                 <p key={index + 5} className="card-text d-flex justify-content-between pt-2">
-                   <small className="text-primary" key={index  + 6}>written on {reviewedHotel.dateWritten}</small>
-                   <small key={index + 7}> {reviewedHotel.timeWritten}</small>
-                </p>
+           <div key={index+1} className=" p-2  d-flex justify-content-center hotel-info-container ">
+            <div className="">
+               <div key={index + 2} className="card shadow-sm book">
+                <div key={index + 3} className="card-body ">
+                  <a key={index + 4}><div className="hotel-name text-white h5">{reviewedHotel.hotelName}</div></a>
+                  <p key={index + 5} className="card-text d-flex justify-content-between pt-2">
+                    <small className="text-primary" key={index  + 6}>written on {reviewedHotel.dateWritten}</small>
+                    <small key={index + 7}> {reviewedHotel.timeWritten}</small>
+                  </p>
+                  </div>
                 </div>
-              </div>
+             </div>
             </div>
          );
        });
 
 
     return (
-      <div>
+      <div className=" card shadow-lg m-4">
         <h2 className="text-center pt-2">Your Reviewed Hotels</h2>
         <div className="results-container d-flex justify-content-center rounded">
-          <div className="row row-cols-2 row-cols-md-2  mb-5 d-flex justify-content-center results-container p-3 ">
+          <div className="row row-cols-2 row-cols-md-2  mb-5 d-flex justify-content-center results-container p-3">
             {reviews}
           </div>
         </div>
+      >
       </div>
     )
   }
